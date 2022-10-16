4832c77583e3bcff4eba4f997185ebb585b9cad2
unknown <aliiobeid01@gmail.com>
2022-10-16 04:36:28 +0300

    backend:add get user profile function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,7 +2,10 @@ const User = require("../models/user.model");
 const Company = require("../models/company.model");
 const Job = require("../models/job.model");
 
-
+const getUser = async (req, res) => {
+  const user = req.user;
+  res.json(user);
+}
 
 module.exports = {
   getUser,
