b52f52cfb6094aa8d80fb1dadda62f9c6674be39
unknown <aliiobeid01@gmail.com>
2022-10-16 04:47:10 +0300

    backend:add get company profile function

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -2,7 +2,10 @@ const User = require("../models/user.model");
 const Company = require("../models/company.model");
 const Job = require("../models/job.model");
 
-
+const getCompany = async (req, res) => {
+  const company = req.company;
+  res.json(company);
+}
 
 module.exports = {
   getCompany,
