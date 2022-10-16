027107dc353069d63c04b73d89a033b3ec867b8c
unknown <aliiobeid01@gmail.com>
2022-10-16 04:46:06 +0300

    backend:create company controller

---
+++ backend/controllers/company.controller.js
@@ -0,0 +1,13 @@
+const User = require("../models/user.model");
+const Company = require("../models/company.model");
+const Job = require("../models/job.model");
+
+
+
+module.exports = {
+  getCompany,
+  addJob,
+  editProfileCompany,
+  getCompanyJobs,
+  getApplicants,
+}
